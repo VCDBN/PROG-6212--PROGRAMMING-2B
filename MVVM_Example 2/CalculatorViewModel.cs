08b636fc18ef13f2d91b653ab91ac3584a37bb82
Denzyl Soobramoney Govender <135228226+Denzylsgovender@users.noreply.github.com>
2023-10-26 12:56:21 +0200

    Add files via upload

---
+++ MVVM_Example 2/CalculatorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Example
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private CalculatorModel _model = new CalculatorModel();
+        private int _number1;
+        private int _number2;
+        private int _result;
+
+        public int Number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                OnPropertyChanged("Number1");
+            }
+        }
+
+        public int Number2
+        {
+            get { return _number2; }
+            set
+            {
+                _number2 = value;
+                OnPropertyChanged("Number2");
+            }
+        }
+
+        public int Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        public ICommand AddCommand => new RelayCommand(AddNumbers);
+
+        private void AddNumbers()
+        {
+            Result = _model.Add(Number1, Number2);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ MVVM_Example 2/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Example
+{
+    public class RelayCommand : ICommand// Implements an interface.
+                                        // This means the class is cable of performing actions specified by the Icommand interface
+    {
+        private Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;// this variable is declared to hold the action to be executed
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;// used to determine whether the command can execute of not
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action();// invokes the action
+        }
+    }
+}
