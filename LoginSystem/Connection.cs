3ed0eddc08e558a4bf4dfc21c3f99d603e3608bf
Denzyl Soobramoney Govender <135228226+Denzylsgovender@users.noreply.github.com>
2023-10-27 13:25:08 +0200

    Add files via upload

---
+++ LoginSystem/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    class Connection
+    {
+        public const String Conn = "Data Source=VCKNDNFTLLP24\\SQLEXPRESS;Initial Catalog=LoginSystemDB;Integrated Security=True";
+    }
+}
